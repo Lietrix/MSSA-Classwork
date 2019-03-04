a929b3279653746fb58ddd1d29e7abe6a3838536
Lietrix <36711347+Lietrix@users.noreply.github.com>
2019-03-04 11:13:00 -0800

    ReadIO

---
+++ FIleIO/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TextFile = "C:\\Users\\WWStudent\\source\\repos\\Classwork\\FIleIO\\RandomPoem.txt";
+            ReadFileIO(TextFile);
+        }
+
+        public static void ReadFileIO(string path)
+        {
+            int blankLines = 0;
+            List<string> Words = new List<string> { };
+            List<string> TextFile = File.ReadAllLines(path).ToList();
+            List<string> DistinctWords = new List<string> { };
+            try
+            {
+                foreach (string line in TextFile)
+                {
+                    if (line.Length < 1) blankLines++;
+                    foreach (string word in line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        Words.Add(word);
+                        if (!DistinctWords.Contains(word)) DistinctWords.Add(word);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("can't open file for reading" + path);
+            }
+            Console.WriteLine($"{TextFile.Count} lines read\n{blankLines} lines blank" +
+                $"\n{Words.Count} total words\n{DistinctWords.Count} Unique Words");
+            foreach(string line in TextFile) Console.WriteLine("\t" + line);
+        }
+    }
+}
